4690e1784a752d9b24fd632276b9522c3702dfc9
ilkin <i.fleydanli@gmail.com>
2020-07-28 12:37:38 +0300

    Changes Home Controller and views

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\QuotesRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
+     * @param  QuotesRepository  $quotesRepository
+     * @return Response
      */
-    public function index()
+    public function index(QuotesRepository $quotesRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'quotes' => $quotesRepository->findAll()
         ]);
     }
 }
